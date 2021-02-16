62a7e54167691c3837e4450d399384d1882236b1
jimixaxa <jimi.kylliainen@koudata.fi>
2021-02-16 14:34:52 +0200

    Initial commit

---
+++ Assets/Game Scripts/NetworkGamePlayerLobby.cs
@@ -0,0 +1,50 @@
+﻿using Mirror;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Jimi.Lobby
+{
+    public class NetworkGamePlayerLobby : NetworkBehaviour
+    {
+        [SyncVar]
+        private string displayName = "Loading...";
+
+
+
+        private NetworkManagerLobby room;
+
+        private NetworkManagerLobby Room
+        {
+            get
+            {
+                if (room != null) { return room; }
+                return room = NetworkManager.singleton as NetworkManagerLobby;
+            }
+        }
+
+
+
+        public override void OnStartClient()
+        {
+            DontDestroyOnLoad(gameObject);
+
+            Room.GamePlayers.Add(this);
+
+        }
+
+        public override void OnStopClient()
+        {
+            Room.GamePlayers.Remove(this);
+
+        }
+
+        [Server]
+        public void SetDisplayName(string displayName)
+        {
+            this.displayName = displayName;
+        }
+
+    }
+}
+
